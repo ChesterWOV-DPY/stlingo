11e10a9e242bc0d0c7cd746bdd64b0d690191430
ChesterWOV <99033489+ChesterWOV-DPY@users.noreply.github.com>
2023-12-10 22:49:35 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const chapterLinks = document.querySelectorAll('button.go');
+
+if (chapterLinks.length === 0) {
+    throw new Error('unable to find chapter links');
+}
+
+for (let link of chapterLinks) {
+    link.addEventListener('click', e => {
+        window.location.assign(`/chapter${e.target.dataset.chapter}.html`)
+    })
+}
